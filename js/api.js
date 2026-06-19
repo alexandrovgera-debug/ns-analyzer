b306c299e9257f9a44e5c996ea86b38c201b95ad
alexandrovgera-debug <alexandrovgera@gmail.com>
2026-06-19 14:59:05 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -13,55 +13,48 @@ export default class NightscoutAPI {
 
         const response = await fetch(url);
 
-        if (!response.ok)
+        if (!response.ok) {
             throw new Error("Ошибка получения devicestatus");
+        }
 
         return await response.json();
     }
-    
-async getProfiles() {
 
-    const url = `${this.baseUrl}/api/v1/profile.json`;
+    async getProfiles() {
 
-    const response = await fetch(url);
+        const url =
+            `${this.baseUrl}/api/v1/profile.json`;
 
-    if (!response.ok) {
-        throw new Error("Ошибка получения profile");
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Ошибка получения profile");
+        }
+
+        const profiles = await response.json();
+
+        if (!profiles.length) {
+            throw new Error("Профили Nightscout не найдены");
+        }
+
+        const root = profiles[0];
+
+        const activeName = root.defaultProfile;
+
+        const active = root.store[activeName];
+
+        if (!active) {
+            throw new Error(`Активный профиль "${activeName}" не найден`);
+        }
+
+        return {
+            basal: active.basal,
+            isf: active.sens,
+            cr: active.carbratio,
+            dia: active.dia,
+            units: active.units,
+            timezone: active.timezone
+        };
     }
-
-    const profiles = await response.json();
-
-    const root = profiles[0];
-
-    const activeName = root.defaultProfile;
-
-    const active = root.store[activeName];
-
-    if (!active) {
-        throw new Error(`Активный профиль "${activeName}" не найден`);
-    }
-
-    return {
-        basal: active.basal,
-        isf: active.sens,
-        cr: active.carbratio,
-        dia: active.dia,
-        units: active.units
-    }
-//}
-
-
-    //async getProfiles() {
-
-        //const url =
-           // `${this.baseUrl}/api/v1/profile.json`;
-
-        //const response = await fetch(url);
-
-        //if (!response.ok)
-            //throw new Error("Ошибка получения profile");
-
-        //return await response.json();
-   // }
 
 }
