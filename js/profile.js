fa161ac1cf06eb87892e4ee25bb7ab72f3c3691b
alexandrovgera-debug <alexandrovgera@gmail.com>
2026-06-19 12:01:44 +0300

    Create profile.js

---
+++ js/profile.js
@@ -0,0 +1,15 @@
+profile = {
+  basal: [
+    { time: "00:00", value: 0.32 },
+    { time: "01:00", value: 0.30 },
+    ...
+  ],
+  isf: [
+    { time: "00:00", value: 5.1 },
+    ...
+  ],
+  cr: [
+    { time: "00:00", value: 12.0 },
+    ...
+  ]
+}
