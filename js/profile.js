698ae26b95f6ca24343ccdda34df786c29230e6a
alexandrovgera-debug <alexandrovgera@gmail.com>
2026-06-19 12:56:45 +0300

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,15 +1,12 @@
 profile = {
   basal: [
     { time: "00:00", value: 0.32 },
-    { time: "01:00", value: 0.30 },
-    ...
+    { time: "01:00", value: 0.30 }
   ],
   isf: [
-    { time: "00:00", value: 5.1 },
-    ...
+    { time: "00:00", value: 5.1 }
   ],
   cr: [
-    { time: "00:00", value: 12.0 },
-    ...
+    { time: "00:00", value: 12.0 }
   ]
 }
